35e17e2a9669b2682bb2f6a41e3afad8f0b99351
mxro <mxro@nowhere.com>
2014-10-07 13:07:44 +1100

    a

---
+++ src/main/java/de/mxro/maven/tools/deploy/DeploymentParameters.java
@@ -0,0 +1,5 @@
+package de.mxro.maven.tools.deploy;
+
+public interface DeploymentParameters {
+
+}
---
+++ src/main/java/de/mxro/maven/tools/deploy/DeploymentParametersBuilder.java
@@ -0,0 +1,10 @@
+package de.mxro.maven.tools.deploy;
+
+public class DeploymentParametersBuilder {
+    
+    
+    public void build() {
+        
+    }
+    
+}
