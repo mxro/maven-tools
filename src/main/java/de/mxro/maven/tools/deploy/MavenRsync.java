e17d560ee1b311859f93e1e4537771cc56aa0e2a
mxro <mxro@nowhere.com>
2014-10-07 13:35:32 +1100

    Improving maven tools, adding capability to write to remote repos Autocommit Tue Oct 07 13:35:32 EST 2014

--- src/main/java/de/mxro/maven/tools/deploy/MavenRsync.java
+++ src/main/java/de/mxro/maven/tools/deploy/MavenRsync.java
@@ -25,68 +25,75 @@ public class MavenRsync {
      * @param newVersion
      * @throws Exception
      */
-    public static void deploy(final DeploymentParameters params) throws Exception {
-    
-        final Path deploymentDir = Files.createTempDirectory("maven-deployment"); // FileSystems.getDefault().getPath("/data/tmp");
-    
-        MavenRemoteRepository.downloadRepositoryXml(params.repositoryRemoteUri(), deploymentDir, params.artifact().groupId(), params
-                .artifact().artifactId());
-    
-        MavenRemoteRepository.assertVersionInRepositoryXml(deploymentDir, params.artifact().version());
-    
-        final Path metadataFile = deploymentDir.resolve("maven-metadata.xml");
-    
-        assert Files.exists(metadataFile);
-    
-        WriteHashes.forFile(metadataFile);
-    
-        final Path versionDir = deploymentDir.resolve(params.artifact().version());
-        Files.createDirectory(versionDir);
-    
-        Path sourceJar = params.forcedSourceJar();
-        if (sourceJar == null) {
-            final Path distributionJar = params.projectDir().resolve(
-                    "target/" + params.artifact().artifactId() + "-" + params.artifact().version()
-                            + "-distribution.jar");
-    
-            if (Files.exists(distributionJar)) {
-                sourceJar = distributionJar;
-            } else {
-                sourceJar = params.projectDir().resolve(
-                        "target/" + params.artifact().artifactId() + "-" + params.artifact().version() + ".jar");
+    public static void deploy(final DeploymentParameters params) {
+        try {
+            final Path deploymentDir = Files.createTempDirectory("maven-deployment"); // FileSystems.getDefault().getPath("/data/tmp");
+
+            MavenRemoteRepository.downloadRepositoryXml(params.repositoryRemoteUri(), deploymentDir, params.artifact()
+                    .groupId(), params.artifact().artifactId());
+
+            MavenRemoteRepository.assertVersionInRepositoryXml(deploymentDir, params.artifact().version());
+
+            final Path metadataFile = deploymentDir.resolve("maven-metadata.xml");
+
+            assert Files.exists(metadataFile);
+
+            WriteHashes.forFile(metadataFile);
+
+            final Path versionDir = deploymentDir.resolve(params.artifact().version());
+            Files.createDirectory(versionDir);
+
+            Path sourceJar = params.forcedSourceJar();
+            if (sourceJar == null) {
+                final Path distributionJar = params.projectDir().resolve(
+                        "target/" + params.artifact().artifactId() + "-" + params.artifact().version()
+                                + "-distribution.jar");
+
+                if (Files.exists(distributionJar)) {
+                    sourceJar = distributionJar;
+                } else {
+                    sourceJar = params.projectDir().resolve(
+                            "target/" + params.artifact().artifactId() + "-" + params.artifact().version() + ".jar");
+                }
             }
+
+            final Path newJar = versionDir.resolve(params.artifact().artifactId() + "-" + params.artifact().version()
+                    + ".jar");
+
+            Files.copy(sourceJar, newJar);
+
+            WriteHashes.forFile(newJar);
+
+            final Path sourcePom = params.projectDir().resolve("pom.xml");
+
+            final byte[] sourcePomBytes = Files.readAllBytes(sourcePom);
+
+            final String sourcePomText = new String(sourcePomBytes, "UTF-8");
+
+            final Path destPom = versionDir.resolve(params.artifact().artifactId() + "-" + params.artifact().version()
+                    + ".pom");
+
+            Files.write(destPom, sourcePomText.replaceAll("<!-- inject-provided-scope -->", "<scope>provided</scope>")
+                    .getBytes("UTF-8"));
+
+            WriteHashes.forFile(destPom);
+
+            final String deploymentPath = params.artifact().groupId().replaceAll("\\.", "/") + "/"
+                    + params.artifact().artifactId();
+
+            final String command = "rsync -avz -e \"ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null\" --progress "
+                    + deploymentDir.toFile().getAbsolutePath()
+                    + "/*"
+                    + " "
+                    + params.rsyncConnectionPath()
+                    + deploymentPath;
+            System.out.println(command);
+
+            System.out.println(Spawn.runBashCommand(command, deploymentDir.toFile()));
+        } catch (final Exception e) {
+            throw new RuntimeException(e);
         }
-    
-        final Path newJar = versionDir.resolve(params.artifact().artifactId() + "-" + params.artifact().version()
-                + ".jar");
-    
-        Files.copy(sourceJar, newJar);
-    
-        WriteHashes.forFile(newJar);
-    
-        final Path sourcePom = params.projectDir().resolve("pom.xml");
-    
-        final byte[] sourcePomBytes = Files.readAllBytes(sourcePom);
-    
-        final String sourcePomText = new String(sourcePomBytes, "UTF-8");
-    
-        final Path destPom = versionDir.resolve(params.artifact().artifactId() + "-" + params.artifact().version()
-                + ".pom");
-    
-        Files.write(destPom, sourcePomText.replaceAll("<!-- inject-provided-scope -->", "<scope>provided</scope>")
-                .getBytes("UTF-8"));
-    
-        WriteHashes.forFile(destPom);
-    
-        final String deploymentPath = params.artifact().groupId().replaceAll("\\.", "/") + "/"
-                + params.artifact().artifactId();
-    
-        final String command = "rsync -avz -e \"ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null\" --progress "
-                + deploymentDir.toFile().getAbsolutePath() + "/*" + " " + params.rsyncConnectionPath() + deploymentPath;
-        System.out.println(command);
-    
-        System.out.println(Spawn.runBashCommand(command, deploymentDir.toFile()));
-    
+
     }
 
 }
