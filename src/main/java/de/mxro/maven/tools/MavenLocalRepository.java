6232189316f5a97d9eb9e4c1e75ab5ebe963b346
mxro <mxro@nowhere.com>
2014-11-18 11:53:52 +1100

    Adding automatic build Autocommit Tue Nov 18 11:53:52 EST 2014

--- src/main/java/de/mxro/maven/tools/MavenLocalRepository.java
+++ src/main/java/de/mxro/maven/tools/MavenLocalRepository.java
@@ -5,17 +5,32 @@ import java.io.File;
 public class MavenLocalRepository {
 
     /**
+     * <p>
      * Deletes the specified artifact from the local repository.
+     * <p>
+     * WARNING: Assumes there is only Maven stuff in the directory. The used
+     * delete method is not save when there are symlinks etc in the directory.
      * 
      * @param localRepositoryRoot
      * @param artifact
      */
     public static void wipeArtifact(final File localRepositoryRoot, final Dependency artifact) {
+        delete(getFolderInLocalRepository(artifact, localRepositoryRoot));
+    }
 
+    private static void delete(final File f) {
+        if (f.isDirectory()) {
+            for (final File c : f.listFiles()) {
+                delete(c);
+            }
+        }
+        if (!f.delete()) {
+            throw new RuntimeException("Could not delete file" + f);
+        }
     }
 
     /**
-     * Determines the path within a local repository for a maven artifact.
+     * Determines the path within a local repository for a Maven artifact.
      * 
      * @param artifact
      * @param localRepositoryRoot
