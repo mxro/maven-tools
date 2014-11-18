d88d0d7b6601b5de0cd61d04a6e151c237da1740
mxro <mxro@nowhere.com>
2014-11-18 11:46:52 +1100

    Adding automatic build Autocommit Tue Nov 18 11:46:52 EST 2014

--- src/test/java/de/mxro/maven/tools/tests/TestLocalRepositoryOperations.java
+++ src/test/java/de/mxro/maven/tools/tests/TestLocalRepositoryOperations.java
@@ -1,13 +1,17 @@
 package de.mxro.maven.tools.tests;
 
+import java.io.File;
+
 import org.junit.Test;
 
+import de.mxro.maven.tools.Dependency;
 import de.mxro.maven.tools.MavenLocalRepository;
 
 public class TestLocalRepositoryOperations {
 
     @Test
     public void test_that_path_determined_correctly() {
-        MavenLocalRepository.getFolderInLocalRepository(artifact, localRepositoryRoot)
+        MavenLocalRepository.getFolderInLocalRepository(Dependency.define("com.appjangle.java", "appjangle-jre",
+                "0.3.6"), new File("/data/databases/localMavenRepository/"));
     }
 }
